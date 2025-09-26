f6f2b28a012ba6ee038b6c46afc82b4b46c2f8c1
ayushshaha <ayushshaha1008@gmail.com>
2025-09-26 17:16:20 +0530

    --- Auto Git Commit ---

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PORTFOLIO/',  // <-- important for GitHub Pages
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
+  base: '/PORTFOLIO/', // required for GitHub Pages
 })
